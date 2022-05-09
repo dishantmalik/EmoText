f4bc14d63d68a72561f4b753b41f3cd04df180a2
dishantmalik <dishantmalik@gmail.com>
2022-05-09 09:49:38 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+# Important to understand the difference between ASCII and Unicode.
+
+# 
+
+# References:
+# https://www.unicode.org/faq/emoji_dingbats.html#2 
+
+
+import re
+
+if __name__ == '__main__':
+   smiley = [':‑)', ':)', ':-]', ':]','8-)','8)',':o)',':c)', ':^)', '=]', '=)' ]
+   laughing = []
+   very_happy = []
+   frown = []
+   crying = []
+   sadness = []
+   surprise = []
+   wink = []
+   straight_face = []
+   embarassed = []
+
+
+   print ("\U0001f600")
